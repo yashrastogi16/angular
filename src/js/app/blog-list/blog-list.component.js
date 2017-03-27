930f66e0dcf31aca794b8e1c2ae376266c683e5e
yashrastogi16 <yash.rastogi916@gmail.com>
2017-03-27 16:27:52 +0530

    added resource for restful api

--- src/js/app/blog-list/blog-list.component.js
+++ src/js/app/blog-list/blog-list.component.js
@@ -2,34 +2,9 @@
 
 angular.module('blogList').
 	component('blogList', {
-		// template: "<div class=''><h1 class='new-class'>{{ title }}</h1><button ng-click='someClickTest()'>Click me!</button></div>",
 		templateUrl: '/templates/blog-list.html',
-		controller: function($scope){
-			var blogItems = [
-			{title: "Title1", id: 1, description: "This is a book"},
-			{title: "Title2", id: 2, description: "This is a holybook"},
-			{title: "Title3", id: 3, description: "This is a textbook"},
-			{title: "Title4", id: 4, description: "This is a notebook"},
-			]
-			$scope.items = blogItems;
-			$scope.title = 'Hello there';
-			$scope.clicks = 0;
-			$scope.someClickTest = function(){
-			console.log('Clicked!');
-			$scope.clicks += 1;
-			$scope.title = "Clicked " + $scope.clicks + " times";
+		controller: function(Post, $routeParams, $scope){
+			$scope.items = Post.query();
+			
 		}
-	}
 	});
-
-	// controller('BlogListController', function($scope){
-	// 	console.log('Hello');
-	// 	$scope.title = 'Hello there';
-	// 	$scope.clicks = 0;
-	// 	$scope.someClickTest = function(){
-	// 		console.log('Clicked!');
-	// 		$scope.clicks += 1;
-	// 		$scope.title = "Clicked " + $scope.clicks + " times";
-
-	// 	}
-	// });
