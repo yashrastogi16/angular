c9ae3891b75f773830e45269b1ecc73fcaea2314
yashrastogi16 <yash.rastogi916@gmail.com>
2017-03-27 18:06:17 +0530

    ng submit functionality

--- src/js/app/blog-detail/blog-detail.component.js
+++ src/js/app/blog-detail/blog-detail.component.js
@@ -12,10 +12,21 @@ angular.module('blogDetail').
 				if(post.id == $routeParams.id){
 					$scope.notFound = false
 					$scope.post = post
+					resetReply()
 				}
 			})
 			})
-
+			$scope.addReply = function(){
+				console.log($scope.reply)
+				$scope.post.comments.push($scope.reply)
+				resetReply()
+			}
+			function resetReply(){
+				$scope.reply = {
+					"id": $scope.post.comments.length + 1,
+					"text": "",
+				}
+			}
 			if($scope.notFound){
 				console.log('Not found')
 				location.path("/404")
